1d3f4129b7cff0c5ad52af7089dbe816085c7556
Simon Fong <simonfong6@gmail.com>
2017-12-13 08:55:46 -0800

    Uploading mnist api projecct

---
+++ course_3/module_3/lesson_1_mnist/server.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from flask import Flask, request
+
+import sys
+import time
+import cv2
+import numpy as np
+import tensorflow as tf
+
+def deepnn(x):
+  """deepnn builds the graph for a deep net for classifying digits.
+  Args:
+    x: an input tensor with the dimensions (N_examples, 784), where 784 is the
+    number of pixels in a standard MNIST image.
+  Returns:
+    A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
+    equal to the logits of classifying the digit into one of 10 classes (the
+    digits 0-9). keep_prob is a scalar placeholder for the probability of
+    dropout.
+  """
+  # Reshape to use within a convolutional neural net.
+  # Last dimension is for "features" - there is only one here, since images are
+  # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+  x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+  # First convolutional layer - maps one grayscale image to 32 feature maps.
+  W_conv1 = weight_variable([5, 5, 1, 32])
+  b_conv1 = bias_variable([32])
+  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+
+  # Pooling layer - downsamples by 2X.
+  h_pool1 = max_pool_2x2(h_conv1)
+
+  # Second convolutional layer -- maps 32 feature maps to 64.
+  W_conv2 = weight_variable([5, 5, 32, 64])
+  b_conv2 = bias_variable([64])
+  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+
+  # Second pooling layer.
+  h_pool2 = max_pool_2x2(h_conv2)
+
+  # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
+  # is down to 7x7x64 feature maps -- maps this to 1024 features.
+  W_fc1 = weight_variable([7 * 7 * 64, 1024])
+  b_fc1 = bias_variable([1024])
+
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+  # Dropout - controls the complexity of the model, prevents co-adaptation of
+  # features.
+  keep_prob = tf.placeholder(tf.float32)
+  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+  # Map the 1024 features to 10 classes, one for each digit
+  W_fc2 = weight_variable([1024, 10])
+  b_fc2 = bias_variable([10])
+
+  y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+  return y_conv, keep_prob
+
+
+def conv2d(x, W):
+  """conv2d returns a 2d convolution layer with full stride."""
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+  """max_pool_2x2 downsamples a feature map by 2X."""
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+
+def weight_variable(shape):
+  """weight_variable generates a weight variable of a given shape."""
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+
+def bias_variable(shape):
+  """bias_variable generates a bias variable of a given shape."""
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+  
+def predict(image):
+
+  # Create the model
+  x = tf.placeholder(tf.float32, [None, 784])
+
+  # Define loss and optimizer
+  y_ = tf.placeholder(tf.float32, [None, 10])
+
+  # Build the graph for the deep net
+  y_conv, keep_prob = deepnn(x)
+  
+  # Create saver object
+  saver = tf.train.Saver()
+
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    saver.restore(sess, "./model.ckpt")
+    image = image.reshape(1,784)
+    feed_dict = {x: image, keep_prob:1.0}
+    classification = sess.run([y_conv],feed_dict)
+    predicted_num = np.argmax(classification[0])
+    message = "We predicted a {}.".format(predicted_num)
+    return message
+
+
+app = Flask(__name__) # create the application instance :)
+
+
+@app.route('/upload/')
+def save_image():
+    file = request.args.get('file')
+    starter = file.find(',')
+    image_data = file[starter+1:]
+    with open('image.jpg', 'wb') as fh:
+        fh.write(image_data.decode('base64'))
+        
+    image = cv2.imread('image.jpg', 0)
+    message = predict(image)
+    return message
+
+
+if(__name__ == "__main__"):
+	app.run(host='0.0.0.0', port=5000, threaded=True)
---
+++ course_3/module_3/lesson_1_mnist/useAPI.py
@@ -0,0 +1,49 @@
+import base64
+import requests
+import numpy as np
+import cv2
+
+# api-endpoint
+URL = "http://localhost:5000/upload"
+
+# mouse callback function
+def draw_dot(event,x,y,flags,param):
+    global img
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.line(img,(x,y),(x,y),(0),1)
+
+# Create an empty image
+img = np.zeros((28,28,1), np.uint8)
+img[:,:,:] = 255
+cv2.namedWindow('image',0)
+
+# Set the draw functionality
+cv2.setMouseCallback('image',draw_dot)
+
+# Let user draw on image until they press 'esc'
+while(1):
+    cv2.imshow('image',img)
+    cv2.resizeWindow('image',1000,1000)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+        
+# Invert the image
+img[img == 0] = 1
+img[img == 255] = 0
+cv2.imwrite('source.jpg',img)
+
+# Reopen image and encode in base64
+image = open('source.jpg', 'rb') #open binary file in read mode
+image_read = image.read()
+image_64_encode = base64.encodestring(image_read)
+
+ 
+# Defining a params dict for the parameters to be sent to the API
+PARAMS = {'file':image_64_encode}
+ 
+# Dending get request and saving the response as response object
+r = requests.get(url = URL, params = PARAMS)
+ 
+# Print response
+data = r.text
+print(data)
