a67196bd19f310998cded8d6eaa07e9be1195c94
Simon Fong <simonfong6@gmail.com>
2018-01-02 07:31:58 -0800

    Adding greengrass code

---
+++ course_3/module_3/lesson_3_greengrass/pub.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import time
+import uuid
+import json
+import logging
+import argparse
+from AWSIoTPythonSDK.core.greengrass.discovery.providers import DiscoveryInfoProvider
+from AWSIoTPythonSDK.core.protocol.connection.cores import ProgressiveBackOffCore
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.exception.AWSIoTExceptions import DiscoveryInvalidRequestException
+
+MAX_DISCOVERY_RETRIES = 10
+GROUP_CA_PATH = "./groupCA/"
+
+host = 'a3f84gpf4vy7nu.iot.us-west-2.amazonaws.com'
+rootCAPath = 'root-ca-cert.pem'
+certificatePath = 'fc662e565e.cert.pem'
+privateKeyPath = 'fc662e565e.private.key'
+clientId = 'HelloWorld_Publisher'
+thingName = 'HelloWorld_Publisher'
+topic = 'hello/world/pubsub'
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Progressive back off core
+backOffCore = ProgressiveBackOffCore()
+
+# Discover GGCs
+discoveryInfoProvider = DiscoveryInfoProvider()
+discoveryInfoProvider.configureEndpoint(host)
+discoveryInfoProvider.configureCredentials(rootCAPath, certificatePath, privateKeyPath)
+discoveryInfoProvider.configureTimeout(10)  # 10 sec
+
+retryCount = MAX_DISCOVERY_RETRIES
+discovered = False
+groupCA = None
+coreInfo = None
+while retryCount != 0:
+    try:
+        discoveryInfo = discoveryInfoProvider.discover(thingName)
+        caList = discoveryInfo.getAllCas()
+        coreList = discoveryInfo.getAllCores()
+
+        # We only pick the first ca and core info
+        groupId, ca = caList[0]
+        coreInfo = coreList[0]
+        print("Discovered GGC: %s from Group: %s" % (coreInfo.coreThingArn, groupId))
+
+        print("Now we persist the connectivity/identity information...")
+        groupCA = GROUP_CA_PATH + groupId + "_CA_" + str(uuid.uuid4()) + ".crt"
+        if not os.path.exists(GROUP_CA_PATH):
+            os.makedirs(GROUP_CA_PATH)
+        groupCAFile = open(groupCA, "w")
+        groupCAFile.write(ca)
+        groupCAFile.close()
+
+        discovered = True
+        print("Now proceed to the connecting flow...")
+        break
+    except DiscoveryInvalidRequestException as e:
+        print("Invalid discovery request detected!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        print("Stopping...")
+        break
+    except BaseException as e:
+        print("Error in discovery!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        retryCount -= 1
+        print("\n%d/%d retries left\n" % (retryCount, MAX_DISCOVERY_RETRIES))
+        print("Backing off...\n")
+        backOffCore.backOff()
+
+if not discovered:
+    print("Discovery failed after %d retries. Exiting...\n" % (MAX_DISCOVERY_RETRIES))
+    sys.exit(-1)
+
+# Iterate through all connection options for the core and use the first successful one
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureCredentials(groupCA, privateKeyPath, certificatePath)
+
+connected = False
+for connectivityInfo in coreInfo.connectivityInfoList:
+    currentHost = connectivityInfo.host
+    currentPort = connectivityInfo.port
+    print("Trying to connect to core at %s:%d" % (currentHost, currentPort))
+    myAWSIoTMQTTClient.configureEndpoint(currentHost, currentPort)
+    try:
+        myAWSIoTMQTTClient.connect()
+        connected = True
+        break
+    except BaseException as e:
+        print("Error in connect!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+
+if not connected:
+    print("Cannot connect to core %s. Exiting..." % coreInfo.coreThingArn)
+    sys.exit(-2)
+
+
+loopCount = 0
+while True:
+    message = {}
+    message['message'] = 'Hello, World! Sent from HelloWorld_Publisher'
+    message['sequence'] = loopCount
+    messageJson = json.dumps(message)
+    myAWSIoTMQTTClient.publish(topic, messageJson, 0)
+    print('Published topic %s: %s\n' % (topic, messageJson))
+    loopCount += 1
+    time.sleep(1)
---
+++ course_3/module_3/lesson_3_greengrass/sub.py
@@ -0,0 +1,141 @@
+import os
+import sys
+import time
+import uuid
+import json
+import logging
+import argparse
+import mraa
+from AWSIoTPythonSDK.core.greengrass.discovery.providers import DiscoveryInfoProvider
+from AWSIoTPythonSDK.core.protocol.connection.cores import ProgressiveBackOffCore
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.exception.AWSIoTExceptions import DiscoveryInvalidRequestException
+
+# General message notification callback
+def customOnMessage(message):
+    #Set GPIO value to 1.
+    #Wait for one second.
+    led.write(1)
+    time.sleep(0.5)
+
+    #Set GPIO value to 0.
+    #Wait for one second.
+    led.write(0)
+    print('Received message on topic %s: %s\n' % (message.topic, message.payload))
+
+MAX_DISCOVERY_RETRIES = 10
+GROUP_CA_PATH = "./groupCA/"
+
+
+host = 'a3f84gpf4vy7nu.iot.us-west-2.amazonaws.com'
+rootCAPath = 'root-ca-cert.pem'
+certificatePath = 'af8586684e.cert.pem'
+privateKeyPath = 'af8586684e.private.key'
+clientId = 'HelloWorld_Subscriber'
+thingName = 'HelloWorld_Subscriber'
+topic = 'hello/world/pubsub'
+
+#Set led to pin 29.
+#29 is GPIO G on grove mezzanine.
+LED_PIN = 29
+
+#Initilize GPIO
+led = mraa.Gpio(LED_PIN)
+
+#Set direction
+led.dir(mraa.DIR_OUT)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Progressive back off core
+backOffCore = ProgressiveBackOffCore()
+
+# Discover GGCs
+discoveryInfoProvider = DiscoveryInfoProvider()
+discoveryInfoProvider.configureEndpoint(host)
+discoveryInfoProvider.configureCredentials(rootCAPath, certificatePath, privateKeyPath)
+discoveryInfoProvider.configureTimeout(10)  # 10 sec
+
+retryCount = MAX_DISCOVERY_RETRIES
+discovered = False
+groupCA = None
+coreInfo = None
+while retryCount != 0:
+    try:
+        discoveryInfo = discoveryInfoProvider.discover(thingName)
+        caList = discoveryInfo.getAllCas()
+        coreList = discoveryInfo.getAllCores()
+
+        # We only pick the first ca and core info
+        groupId, ca = caList[0]
+        coreInfo = coreList[0]
+        print("Discovered GGC: %s from Group: %s" % (coreInfo.coreThingArn, groupId))
+
+        print("Now we persist the connectivity/identity information...")
+        groupCA = GROUP_CA_PATH + groupId + "_CA_" + str(uuid.uuid4()) + ".crt"
+        if not os.path.exists(GROUP_CA_PATH):
+            os.makedirs(GROUP_CA_PATH)
+        groupCAFile = open(groupCA, "w")
+        groupCAFile.write(ca)
+        groupCAFile.close()
+
+        discovered = True
+        print("Now proceed to the connecting flow...")
+        break
+    except DiscoveryInvalidRequestException as e:
+        print("Invalid discovery request detected!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        print("Stopping...")
+        break
+    except BaseException as e:
+        print("Error in discovery!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        retryCount -= 1
+        print("\n%d/%d retries left\n" % (retryCount, MAX_DISCOVERY_RETRIES))
+        print("Backing off...\n")
+        backOffCore.backOff()
+
+if not discovered:
+    print("Discovery failed after %d retries. Exiting...\n" % (MAX_DISCOVERY_RETRIES))
+    sys.exit(-1)
+
+# Iterate through all connection options for the core and use the first successful one
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureCredentials(groupCA, privateKeyPath, certificatePath)
+myAWSIoTMQTTClient.onMessage = customOnMessage
+
+connected = False
+for connectivityInfo in coreInfo.connectivityInfoList:
+    currentHost = connectivityInfo.host
+    currentPort = connectivityInfo.port
+    print("Trying to connect to core at %s:%d" % (currentHost, currentPort))
+    myAWSIoTMQTTClient.configureEndpoint(currentHost, currentPort)
+    try:
+        myAWSIoTMQTTClient.connect()
+        connected = True
+        break
+    except BaseException as e:
+        print("Error in connect!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+
+if not connected:
+    print("Cannot connect to core %s. Exiting..." % coreInfo.coreThingArn)
+    sys.exit(-2)
+
+# Successfully connected to the core
+
+myAWSIoTMQTTClient.subscribe(topic, 0, None)
+time.sleep(2)
+
+loopCount = 0
+while True:
+    time.sleep(1)
